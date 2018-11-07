1b4b6cffe5829b85b74727e3acba8b45a20c6518
yangtaoqibao <1821470255@qq.com>
2018-11-07 18:58:34 +0800

    fsdf

--- PHPDailyTest/11-7/a.php
+++ PHPDailyTest/11-7/a.php
@@ -1,45 +1,96 @@
 <?php
 
+
+function chou($num){
+  for($i = 2;;$i*=1){
+    if(is_int($num/2)){
+      $num = $num/2;
+      echo $num;
+    }else if($num == 1){
+      return '是丑数';
+    }else{
+      for($j = 3;;$j*=1){
+        if(is_int($num/$j)){
+          $num = $num/$j;
+        }else if($num == 1){
+          return '是丑数';
+        }else{
+          for($o = 5;;$o*=1){
+            if(is_int($num/$o)){
+              $num = $num/$o;
+            }else if($num == 1){
+              return '是丑数';
+            }else{
+              return '您输入的不是丑数';
+            }
+          }
+        }
+      }
+    }
+
+  }
+}
+
+echo chou(16);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // $number=0;
 // function ab($a,$b){
 // 	$number
 // }
 
-echo ugly(25);
-function ugly($ugly)
-{
-    if($ugly==1)return 1;
-    if(getUgly($ugly)==false)return "不是丑数";
-    $j = 1;
-    for($i=2;;$i++)
-    {
-        $a = getUgly($i);
-        if($a){
-            $j++;
-        }
-        if($i==$ugly){
-            return $j;
-        }
-    }
-}
-function getUgly($ugly)
-{
-   if($ugly<=0) return false;
-   while($ugly!=1)
-   {
-       while($ugly%2==0)
-       {
-           $ugly = $ugly/2;
-       }
-       while($ugly%3==0){
-           $ugly = $ugly/3;
-       }
-       while($ugly%5==0){
-           $ugly = $ugly/5;
-       }
-       return $ugly==1?true:false;
-   }
-}
+// echo ugly(25);
+// function ugly($ugly)
+// {
+//     if($ugly==1)return 1;
+//     if(getUgly($ugly)==false)return "不是丑数";
+//     $j = 1;
+//     for($i=2;;$i++)
+//     {
+//         $a = getUgly($i);
+//         if($a){
+//             $j++;
+//         }
+//         if($i==$ugly){
+//             return $j;
+//         }
+//     }
+// }
+// function getUgly($ugly)
+// {
+//    if($ugly<=0) return false;
+//    while($ugly!=1)
+//    {
+//        while($ugly%2==0)
+//        {
+//            $ugly = $ugly/2;
+//        }
+//        while($ugly%3==0){
+//            $ugly = $ugly/3;
+//        }
+//        while($ugly%5==0){
+//            $ugly = $ugly/5;
+//        }
+//        return $ugly==1?true:false;
+//    }
+// }
 
 
 
