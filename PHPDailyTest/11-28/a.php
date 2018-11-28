36f9997faf48c87f95c2a4707eb59cba70fa9d08
yangtaoqibao <1821470255@qq.com>
2018-11-28 09:28:29 +0800

    faf

---
+++ PHPDailyTest/11-28/a.php
@@ -0,0 +1,31 @@
+<?php?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<style>
+	.a{
+		width: 200px;
+		height: 100px
+		;background-color: red;
+		animation:myfirst 2s;
+	}
+		@keyframes myfirst
+			{	
+		0% {background:red;}
+		25%{background:yellow;}
+		50%{background:blue;}
+		70%{background:green;}
+		100% {background:grey;}
+		}
+
+</style>
+<body>
+	<div class="a" style="">MyfirstFlash</div>
+
+</body>
+</html>
---
+++ PHPDailyTest/11-28/kao.php
@@ -0,0 +1,37 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div style="height: 200px;width: 100px;color: yellow;background:red;">
+		<h4>MyfirstFlash</h4>
+		<p>动画时长为10s，播放次数是1只支持Safari和Chrome浏览器</p>
+	</div>
+	<p>两秒后</p>
+	<div style="height: 200px;width: 100px;color: red;background:yellow;">
+		<h4>MyfirstFlash</h4>
+		<p>动画时长为10s，播放次数是1只支持Safari和Chrome浏览器</p>
+	</div>
+	<p>两秒后</p>
+	<div style="height: 200px;width: 100px;color: red;background:#1200ff;">
+		<h4>MyfirstFlash</h4>
+		<p>动画时长为10s，播放次数是1只支持Safari和Chrome浏览器</p>
+	</div>
+	<p>两秒后</p>
+	<div style="height: 200px;width: 100px;color: red;background:green;">
+		<h4>MyfirstFlash</h4>
+		<p>动画时长为10s，播放次数是1只支持Safari和Chrome浏览器</p>
+	</div>
+	<p>两秒后</p>
+	<div style="height: 200px;width: 100px;color: red;background:#b9b3bb;">
+		<h4>MyfirstFlash</h4>
+		<p>动画时长为10s，播放次数是1只支持Safari和Chrome浏览器</p>#b9b3bb
+	</div>
+</body>
+</html>
